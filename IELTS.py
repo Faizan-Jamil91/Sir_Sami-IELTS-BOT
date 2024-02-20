06c02048eb2546a8c1f7890ac0f8b11b0afd20a7
Faizan Jamil <141227220+Faizan-Jamil91@users.noreply.github.com>
2024-02-20 14:09:43 +0500

    Update IELTS.py

--- IELTS.py
+++ IELTS.py
@@ -98,7 +98,7 @@ body {
 
 st.markdown(custom_css, unsafe_allow_html=True)
 
-st.markdown("<h1 class='title'>Sir Sami Ielts Bot</h1>", unsafe_allow_html=True)
+st.markdown("<h1 class='title'>Sir Sami Ielts Expert</h1>", unsafe_allow_html=True)
 
 name = st.text_input("Please enter your full name:")
 email_address = st.text_input("Please enter your email address:")
